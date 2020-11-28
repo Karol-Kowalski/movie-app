390d5660d3a81ad5f3baa53556b8678b7576b408
Virion <kajko195@tlen.pl>
2020-11-28 11:53:36 +0100

    Favourites movies feature
    Implement feature favourites movies. User can add and remove films from
    favourites. Favourites collection is stored in local storage.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,58 @@ import './App.css';
 import MovieList from './components/MovieList';
 import SearchBox from './components/SearchBox';
 import MovieListHeading from './components/MovieListHeading';
-import AddFavourite from './components/AddFavourites';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [favourites, setFavourites] = useState([])
   const [searchValue, setSearchValue] = useState('')
-
+  
+  
   const getMoviesApi = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=82131e8e`
-
+    
     const response = await fetch(url)
     const responseJson = await response.json()
-
+    
     if (responseJson.Search) {
       setMovies(responseJson.Search)
     }
   }
-
+  
   useEffect(() => {
     getMoviesApi(searchValue)
   },[searchValue])
+
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+        localStorage.getItem('react-movie-app-favourites')
+    )
+    if(movieFavourites) {
+      setFavourites(movieFavourites)
+    }
+  },[])
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  }
+
+  const addFavouritesMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie]
+    setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
+  }
+
+  const removeFavouritesMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    )
+
+    setFavourites(newFavouriteList)
+    saveToLocalStorage(newFavouriteList)
+  }
+
 
   return (
     <div className="container-fluid movie-app">
@@ -31,8 +63,24 @@ const App = () => {
         <MovieListHeading heading='Movies' />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
-      <div className='row d-flex'>
-        <MovieList movies={movies} favouriteComponent={AddFavourite}/>
+      <div className='row'>
+        <MovieList 
+            movies={movies} 
+            favouriteComponent={AddFavourites}
+            handleFavouriteClick={addFavouritesMovie}
+        />
+      </div>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favourites' />
+      </div>
+      <div className='row'>
+        {favourites &&
+          <MovieList 
+            movies={favourites} 
+            favouriteComponent={RemoveFavourites}
+            handleFavouriteClick={removeFavouritesMovie}
+          />
+        }
       </div>
     </div>
   );
